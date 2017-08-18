83d2f068b4cab80ef2cce43677aadb64b9599efb
hweiming <710564672@qq.com>
2017-08-18 14:34:38 +0800

    create a hello.c by huangweiming

---
+++ hello.c
@@ -0,0 +1,21 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2017 Huang Weiming<710564672@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  hello.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(2017年08月18日)
+ *         Author:  Huang Weiming <710564672@qq.com>
+ *      ChangeLog:  1, Release initial version on "2017年08月18日 14时30分01秒"
+ *                 
+ ********************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    printf ("hello\n");
+    return 0;
+}
